020e73512050f24f0e86705c66eda77a54723675
sujathabharathi <30913269+sujathabharathi@users.noreply.github.com>
2018-02-25 23:45:56 +1300

    Add files via upload

---
+++ jsquery/videoplay.js
@@ -0,0 +1,34 @@
+function Pause1()
+{ 
+  myVideo1.pause(); 
+}
+function Play1()
+{
+if (myVideo1.paused) 
+  myVideo1.play();     
+} 
+
+
+
+
+function Pause2()
+{ 
+  myVideo2.pause(); 
+}
+function Play2()
+{
+if (myVideo2.paused) 
+  myVideo2.play();     
+} 
+
+
+
+function Pause3()
+{ 
+  myVideo3.pause(); 
+}
+function Play3()
+{
+if (myVideo3.paused) 
+  myVideo3.play();     
+} 
